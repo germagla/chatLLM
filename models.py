2ef5c39f3ed34247330fe5e100cfd6a083881a49
Gerard Magla <gmagladev@gmail.com>
2023-09-17 15:38:31 +0000

    Create Conversation and Message models, Register aforementioned models to admin

--- models.py
+++ models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Conversation(models.Model):
+    date_started = models.DateField()
+    title = models.CharField()
+
+    def __str__(self):
+        return self.title
+
+    def __len__(self):
+        return self.message_set.count()
+
+
+class Message(models.Model):
+    text = models.TextField()
+    time_sent = models.DateTimeField()
+    role = models.CharField()
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+    def __len__(self):
+        return len(self.text)
